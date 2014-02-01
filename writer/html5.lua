87ccfe1e47fee36a681a551c7a476c6c1c0df44f
Alex Szczuczko <alex@szc.ca>
2014-02-01 08:26:52 -0500

    Add indentation to DefinitionList

--- writer/html5.lua
+++ writer/html5.lua
@@ -315,10 +315,9 @@ end
 -- Revisit association list STackValue instance.
 function DefinitionList(items)
     local buffer = {}
-    -- TODO indent
     for _,item in pairs(items) do
         for k, v in pairs(item) do
-            table.insert(buffer,"<dt>" .. k .. "</dt>\n<dd>" .. table.concat(v,"</dd>\n<dd>") .. "</dd>")
+            table.insert(buffer,"    <dt>" .. k .. "</dt>\n    <dd>" .. table.concat(v,"</dd>\n    <dd>") .. "</dd>")
         end
     end
     return "<dl>\n" .. table.concat(buffer, "\n") .. "\n</dl>"
